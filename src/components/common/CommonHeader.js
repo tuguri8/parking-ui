e9dd2cb33dcabdcafbf7ebd80626e0f792ba8c34
한승우 <tuguri8@gmail.com>
2019-09-30 16:59:59 +0900

    네브바수정

--- src/components/common/CommonHeader.js
+++ src/components/common/CommonHeader.js
@@ -1,15 +1,18 @@
 import React, {Component} from 'react';
 import {inject, observer} from 'mobx-react';
 
-import {PageHeader, Typography, Menu, Icon} from "antd";
+import {PageHeader, Typography, Menu, Row, Col} from "antd";
+import { NavBar, Icon } from 'antd-mobile';
+
 import './CommonHeader.scss'
+import 'antd-mobile/dist/antd-mobile.css';
 
 @observer
 class CommonHeader extends Component {
   render() {
     return (
       <React.Fragment>
-        <PageHeader className="header" title={<Typography.Text className="header-text">주차장 상세 정보</Typography.Text>}/>
+        <NavBar className="header" mode="light" icon={<Icon className="icon" type="left" size={"lg"}/>} onLeftClick={() => window.close()}>주차장 상세 정보</NavBar>
       </React.Fragment>
     );
   }
